846ddb2e7ea8824e06eba63a7c4a00e482948404
liang <shanghliang@163.com>
2015-11-06 11:53:03 +0800

    init

---
+++ foundation/src/com/maneater/foundation/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.maneater.foundation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping("login")
+	public String login() {
+		return "/admin/login";
+	}
+
+	@RequestMapping("index")
+	public String index() {
+		return "/admin/index";
+	}
+}
